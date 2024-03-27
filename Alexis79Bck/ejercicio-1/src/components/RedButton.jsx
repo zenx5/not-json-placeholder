c28370dc8a1b1d80d28368e72b4de4c3af5bd664
Alexis E. Mata <alexedmata@gmail.com>
2024-03-27 13:59:05 -0400

    Ejercicio 1 terminado

--- Alexis79Bck/ejercicio-1/src/components/RedButton.jsx
+++ Alexis79Bck/ejercicio-1/src/components/RedButton.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
 
-export default function RedButton() {
+export default function RedButton(props) {
   return (
     <button 
-        type="button" 
+        
         className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+        {...props}
     >
         Random Imagen Anime
     </button>
