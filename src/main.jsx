9e782cbbf7858ae19eed605a196182a6b3c404bb
Octavio Martinez <omartinez1618@gmail.com>
2024-04-05 23:20:24 -0400

    router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './views/Home'
+import Contact from './views/Contact'
+import PostDetails from './components/PostDetails'
 import './index.css'
+import Layout from './views/Layout'
+
+
+const routes = createBrowserRouter([
+  { path: '/', element: <Layout><Home /></Layout> },
+  { path: '/contact', element: <Layout><Contact /></Layout> },
+  {
+    path: '/posts',
+    children: [
+      { path: ':slug', element: <Layout><PostDetails /></Layout> }
+    ]
+  }
+])
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <h1 className="text-center text-3xl my-5">React Router Example</h1>
+    <RouterProvider router={routes} />
   </React.StrictMode>,
 )
