731ffed75e663ac49422475eb134e11c224d53af
Javier Martínez <143547061+Javierjmc@users.noreply.github.com>
2024-04-13 01:59:35 +0000

    usando usePosts

--- src/views/CommentDetails.jsx
+++ src/views/CommentDetails.jsx
@@ -1,11 +1,17 @@
 import { NavLink } from "react-router-dom"
+import {  usePosts } from '../context/PostProvider'
 
 export default function CommentDetails(  ) {
+  
+  const { posts } = usePosts()
+
   
   return (
     <div className="py-4">
       
       <h4 className="font-bold">Titulo</h4>
+      { posts }
+      
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic quam velit odio expedita assumenda accusantium reiciendis odit obcaecati rem earum vero tempora, perspiciatis dolor numquam at veniam! Ipsam, nisi consequuntur?</p>
       <NavLink to='/posts' className="block text-blue-500 hover:underline">Volver</NavLink>
     </div>
