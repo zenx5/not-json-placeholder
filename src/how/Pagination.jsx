f2b25faf164bf1f4575377bf93449cf23c5820ee
Octavio Martinez <omartinez1618@gmail.com>
2024-05-09 19:37:40 -0400

    paginacion

--- src/how/Pagination.jsx
+++ src/how/Pagination.jsx
@@ -1,10 +1,12 @@
 export default function Pagination() {
 
     return <div className="px-20 py-2 text-slate-200">
-        <h2 className="text-xl my-4 font-bold text-white">Componente para Paginacion</h2>
-        <p>
-            Este sera un componente que nos permitira navegar entre las paginas.
-        </p>
+        <h2 className="text-xl mt-4 mb-3 font-bold text-white">Componente para Paginacion</h2>
+        <div className="h-[10rem] overflow-y-scroll px-2" style={{ scrollbarWidth:'thin' }}>
+            <div className="flex flex-col items-center">
+
+            </div>
+        </div>
 
     </div>
 }
