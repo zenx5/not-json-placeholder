d041b2244abfe133ec9f4be4191d5efce40efb20
Octavio Martinez <omartinez1618@gmail.com>
2024-05-09 16:37:16 -0400

    albums ajustes

--- src/components/AlbumDetails.jsx
+++ src/components/AlbumDetails.jsx
@@ -1,30 +1,27 @@
-import React from "react";
 import { NavLink } from "react-router-dom"
 import { useParams } from "react-router-dom";
 import { useData } from "../context/DataProvider";
 
-const AlbumDetails = () => {
+export default function AlbumDetails(){
   const { albumId } = useParams();
-  const { albums } = useData();
-  const { photos } = useData()
-  
+  const { albums , photos } = useData()
 
-  const selectedAlbum = albums.find((album) => album.id === parseInt(albumId));
- 
+
+  const selectedAlbum = albums.find((album) => album.id === parseInt(albumId))
+
   return (
     <div>
       <span className="flex flex-row justify-between items-center mt-10 mb-5">
           <h1 className="font-bold text-xl">Fotos del Álbum {selectedAlbum.id}</h1>
-          <NavLink to="/albums" className="block text-blue-500 hover:underline">Volver</NavLink>  
+          <NavLink to="/albums" className="block text-blue-500 hover:underline">Volver</NavLink>
       </span>
-        
       <div className="px-4 lg:px-0 grid gtid-cols-1 sm:grid-cols-3 gap-4">
-            {photos && photos.filter(valor=>valor.albumId==selectedAlbum.id).map((photo) => (
-            <img key={photo.id} src={photo.url} alt={photo.title} className="w-full h-auto" />
+            {photos && photos.filter(valor=>valor.albumId==selectedAlbum.id).map((photo) => (<div key={photo.id} className="relative">
+              <img src={photo.url} alt={photo.title} className="w-full h-auto border border-slate-200" />
+              <span className="flex flex-col justify-end absolute bottom-0 px-2 py-1 h-[80px] w-full bg-slate-800 bg-opacity-50 text-white ">{photo.title}</span>
+            </div>
             ))}
         </div>
     </div>
-  );
-};
-
-export default AlbumDetails;
+  )
+}
